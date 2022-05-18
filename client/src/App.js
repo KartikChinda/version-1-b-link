9c85d717179f3334330ee65b8ae22a8b760ec4db
Kirsten H <krch.1598+github@gmail.com>
2022-05-18 15:35:50 +0200

    Add routes for Sign Up and Log In

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import SignUp from './Components/Signup/Signup';
+import LogIn from './Components/Signup/Login';
 const App = () => {
   return (
     <div>
-      <SignUp></SignUp>
+      <div className="App">
+        <Routes>
+          <Route
+            path="/signup"
+            element={
+              <SignUp></SignUp>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <LogIn></LogIn>
+            }
+          />
+        </Routes>
+      </div>
     </div>
   );
 };
