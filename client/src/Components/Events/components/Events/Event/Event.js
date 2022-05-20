e41c3cd374957a583bfa4dfd31c498f818e2f149
zzhilin <zhilin_li@outlook.com>
2022-05-20 05:40:57 -0400

    fixed event grid

--- client/src/Components/Events/components/Events/Event/Event.js
+++ client/src/Components/Events/components/Events/Event/Event.js
@@ -8,34 +8,34 @@ import Typography from '@mui/material/Typography';
 import image from '../../../../../images/eventcard.png';
 
 const Event = (props) => {
-    return (
-        <Card sx={{ maxWidth: 300 }}>
-            <CardMedia
-                component="img"
-                height="140"
-                image={image}
-                alt="green iguana"
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                    Test Event
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    B-Link
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    11-Nov-2023
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo, aspernatur.
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Join Chat</Button>
-
-            </CardActions>
-        </Card>
-    );
-}
+  return (
+    <Card sx={{ maxWidth: 400 }}>
+      <CardMedia
+        component='img'
+        height='140'
+        image={image}
+        alt='green iguana'
+      />
+      <CardContent>
+        <Typography gutterBottom variant='h5' component='div'>
+          Test Event
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          B-Link
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          11-Nov-2023
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo,
+          aspernatur.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size='small'>Join Chat</Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 export default Event;
